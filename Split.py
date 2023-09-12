9b590c1639e30469a6c13950f51084791676009a
bossesimon <bossesim@hu-berlin.de>
2023-09-12 13:50:57 +0200

    Add regression for split/merge-processes and calculate minimum split parameter to reduce runtime
    Used nnls for regression in Create_split_estimation_model to avoid negative weights or bias

--- Split.py
+++ Split.py
@@ -1,2 +1,41 @@
-def split(DAW):
-    return
+import numpy as np
+import math
+
+#returns a degree of parallelism from which splitting is reducing runtime
+def min_beneficial_split(alignment_task, median_input_size, annotation_database, infra_name):
+    align_model = annotation_database.runtime_estimation_model[infra_name][alignment_task.tool] 
+    align_model_weight = align_model.coef_[0]
+    align_model_bias = align_model.intercept_
+    
+    split_model = annotation_database.runtime_estimation_model[infra_name]["split_merge"]
+
+    #estimate time it takes to split
+    split_time = split_model[0] * median_input_size + split_model[1]
+    
+    #estimate alignment runtime without splitting
+    align_time_no_split = align_model.predict(median_input_size.reshape(-1, 1))[0]
+    
+    #max alignment runtime after splitting to reduce runtime overall
+    max_align_time_split = align_time_no_split - split_time
+    
+    #get input datasize for max alignment runtime after splitting and calculate min split parameter
+    max_input_size_split = (max_align_time_split - align_model_bias)/align_model_weight
+    min_split = math.ceil(median_input_size/max_input_size_split)
+
+    #return ReLU of min_split, when min_split is negative, splitting increases runtime
+    return min_split * (min_split > 0)
+
+
+def split(DAW, annotation_database):
+
+    alignment_task = next(task for task in DAW.tasks if task.operation == "align")
+    median_input_size = np.array(0.4) #TODO: get real median input size, so far input obj of DAW is empty
+    infra_name = "kubernetes-cluster"#TODO: infra may need an identifier to get corresponding entries from the annotation database?
+    min_beneficial_split(alignment_task, median_input_size, annotation_database, infra_name)
+    get_split_somayeh = min_beneficial_split #TODO: implement real approach for getting number of chunks here
+    #TODO: implement changes in DAW object
+    
+    return DAW
+    
+    
+
--- annotation.py
+++ annotation.py
@@ -1,5 +1,6 @@
 
 from sklearn.linear_model import LinearRegression
+from scipy.optimize import nnls
 from matplotlib import pyplot as plt
 import json
 import pandas as pd
@@ -9,10 +10,11 @@ import os
 class AnnotationDB:  
 
     annotation_db:list = []
-    rumtime_estimation_model:object = None
+    runtime_estimation_model:object = None
+
 
     @staticmethod
-    def Create_runtime_estimation_model(runtime_measured):
+    def Create_alignment_runtime_estimation_model(runtime_measured):
         #TODO: add kallisto when completed
 
         df_runtime = pd.read_csv(runtime_measured, delimiter=",")
@@ -27,8 +29,8 @@ class AnnotationDB:
             for aligner in aligners:
                 df = pd.DataFrame({'reference_sizes': df_infra["dataset_size"], 'runtime': df_infra[aligner] })
                 X = df.iloc[:,:-1].values # feature matrix: reference_sizes
-                y = df.iloc[:,1].values # response vector: ram_used
-                model = LinearRegression()
+                y = df.iloc[:,1].values # response vector: time_used
+                model = LinearRegression(positive=True)
                 model.fit(X, y)
                 #plt.scatter(X, y)
                 #plt.plot(X, model.predict(X)
@@ -37,15 +39,48 @@ class AnnotationDB:
             list_models[infra] = this_infra
         return list_models
 
+    @staticmethod
+    def Create_split_runtime_estimation_model(runtime_measured):
+
+        df_runtime = pd.read_csv(runtime_measured, delimiter=",")
+
+        infrastructures = df_runtime["infrastructure"].unique()
+
+        list_models = {}
+        for infra in infrastructures: 
+            df_infra = df_runtime.loc[df_runtime["infrastructure"] == infra]
+            df = pd.DataFrame({'reference_sizes': df_infra["dataset_size"], 'runtime': df_infra["duration"] })
+            X = df.iloc[:,:-1].values # feature matrix: reference_sizes
+            y = df.iloc[:,1].values # response vector: time_used
+            
+            #use nnls for non-negative weights
+            model = nnls(X, y)
+            model = (model[0][0], model[1])
+            
+            list_models[infra] = model
+        return list_models
+
+
+
     def __init__    (self, annotation_files_list):
         
-        path_runtimes = "./annotation_files/runtime_aligners.csv"
-        if( os.path.isfile(path_runtimes) == False):
-            raise Exception("File missing: "+path_runtimes) 
+        path_runtimes_align = "./annotation_files/runtime_aligners.csv"
+        if( os.path.isfile(path_runtimes_align) == False):
+            raise Exception("File missing: "+path_runtimes_align) 
         else:
-            with open(path_runtimes) as mfile:
-                self.rumtime_estimation_model = self.Create_runtime_estimation_model(mfile)
-        annotation_db = []
+            with open(path_runtimes_align) as mfile:
+                self.runtime_estimation_model = self.Create_alignment_runtime_estimation_model(mfile)  
+                 
+        path_runtimes_split = "./annotation_files/runtime_split_merge.csv"
+        if( os.path.isfile(path_runtimes_split) == False):
+            raise Exception("File missing: "+path_runtimes_split) 
+        else:
+            with open(path_runtimes_split) as mfile:
+                split_estimators = self.Create_split_runtime_estimation_model(mfile)   
+        for infra in split_estimators:
+        	self.runtime_estimation_model[infra]["split_merge"] = split_estimators[infra]                          
+        print(self.runtime_estimation_model)
+        annotation_db = []        
         for file_path in annotation_files_list:
             with open(file_path) as json_file:
                 print(file_path)
